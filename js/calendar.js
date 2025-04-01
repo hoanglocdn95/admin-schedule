33bbd517b7f90820fa9e57092475815ee912161e
Trần Hoàng Lộc <hoanglocdn95@Trans-Laptop.local>
2025-04-01 08:56:24 +0700

    implement modaol arrange time

--- js/calendar.js
+++ js/calendar.js
@@ -1,6 +1,5 @@
 const SCHEDULE_API_URL =
-  "https://script.google.com/macros/s/AKfycbzPRg-axjCT4R-6tDV5N9PcJ3VIiHk0EOKobk9Pbj4qgayIDkWrFautirWm7dJo3ejpkA/exec";
-
+  "https://script.google.com/macros/s/AKfycbz7yAeSFp7U_j18HpyHvXOu2QBeEJMf2V8-uc2jOcBgcY7LaUAjUS9HkS3zauDH9FoHtQ/exec";
 const ACCOUNT_API_URL =
   "https://script.google.com/macros/s/AKfycbytD-SM2ZqlC_8ZealODSCynhWbauexL3JENPDczagcC_B5zcz2Epn8cOqxbQQ3w-Zijw/exec";
 
@@ -10,12 +9,22 @@ let studentData = [];
 let trainerData = [];
 
 let studentCalendar = {};
+let trainerCalendar = {};
 
 const dayOfCurrentWeek = [];
+
+let scheduleQuantity = 1;
 
 let currentScheduledData = Array.from({ length: 3 }, () => Array(7).fill(""));
 
 const useInfo = JSON.parse(sessionStorage.getItem("user_info"));
+
+if (!sessionStorage.getItem("user_email")) {
+  window.location.href = "index.html";
+}
+if (!sessionStorage.getItem("user_info")) {
+  window.location.href = "admin.html";
+}
 
 function parseSchedule(allData) {
   const result = {};
@@ -51,15 +60,6 @@ function parseSchedule(allData) {
 
   return result;
 }
-
-document.addEventListener("DOMContentLoaded", function () {
-  if (!sessionStorage.getItem("user_email")) {
-    window.location.href = "index.html";
-  }
-  if (!sessionStorage.getItem("user_info")) {
-    window.location.href = "admin.html";
-  }
-});
 
 function logout() {
   sessionStorage.clear();
@@ -158,10 +158,98 @@ function generateTableBody() {
   });
 }
 
+function generateRegisterClass(count) {
+  const registerClassContainer = document.getElementById("register-class");
+  registerClassContainer.innerHTML = "";
+
+  Array(count)
+    .fill("")
+    .map((item) => {
+      const settingSession = `
+              <div>
+                Từ
+                <input type="time" style="width: fit-content" />
+                đến
+                <input type="time" style="width: fit-content" />
+                <select class="browser-default choose-trainer">
+                  <option value="" disabled selected>Chọn Trainer</option>
+                  ${trainerData
+                    .map((tr) => {
+                      return `<option value="${tr.name}">${tr.name}</option>`;
+                    })
+                    .join("")}
+                </select>
+              </div>
+            `;
+      registerClassContainer.innerHTML += settingSession;
+    });
+}
+
+function generateScheduleEdit(index, day) {
+  const scheduleContainer = document.getElementById("modal-schedule");
+  scheduleContainer.innerHTML = "";
+
+  const leisureTime = studentData[index].times[day].map((t) => t.split(","));
+
+  leisureTime.flat().forEach((time, i) => {
+    console.log(" leisureTime.flat ~ time:", time);
+    const [start, end] = time.split("-");
+
+    const row = document.createElement("div");
+    row.innerHTML = `
+          <label>Khung giờ ${i + 1}:</label>
+          Từ
+          <input type="time" style="width: fit-content" value="${start.trim()}"/>
+          đến
+          <input type="time" style="width: fit-content" value="${end.trim()}" />
+          <button>Delete</button>
+        `;
+    scheduleContainer.appendChild(row);
+  });
+}
+
+function generateSchedule(index, day) {
+  const scheduleContainer = document.getElementById("modal-schedule");
+  scheduleContainer.innerHTML = "";
+
+  const leisureTime = studentData[index].times[day].map((t) => t.split(","));
+
+  scheduleQuantity = leisureTime.flat().length;
+
+  leisureTime.flat().forEach((time, i) => {
+    const [start, end] = time.split("-");
+
+    const row = document.createElement("div");
+    row.innerHTML = `
+          <label>Khung giờ ${i + 1}:</label>
+          <p>Từ <b>${start.trim()}</b> đến <b>${end.trim()}</b></p>
+        `;
+    scheduleContainer.appendChild(row);
+  });
+}
+
+function addSchedule() {
+  const scheduleContainer = document.getElementById("modal-schedule");
+
+  scheduleQuantity += 1;
+
+  const row = document.createElement("div");
+  row.innerHTML = `
+          <label>Khung giờ ${scheduleQuantity}:</label>
+          Từ
+          <input type="time" style="width: fit-content" />
+          đến
+          <input type="time" style="width: fit-content"  />
+          <button>Delete</button>
+        `;
+  scheduleContainer.appendChild(row);
+}
+
 document.addEventListener("DOMContentLoaded", async function () {
   const loadingOverlay = document.getElementById("loadingOverlay");
   loadingOverlay.style.display = "flex";
-  await Promise.all([getAllUser(), initTableData()]);
+
+  await Promise.all([getAllUser(), getStudentCalendar(), getTrainerCalendar()]);
 
   generateHeaders();
   generateTableBody();
@@ -181,34 +269,27 @@ document.addEventListener("DOMContentLoaded", async function () {
       document.getElementById("modal-email").innerText = email;
       document.getElementById("modal-time").innerText = dayOfCurrentWeek[day];
 
-      // Xóa lịch học cũ
-      const scheduleContainer = document.getElementById("modal-schedule");
-      scheduleContainer.innerHTML = "";
-
-      // Thêm input thời gian
-      studentData[index].times[day].forEach((time, index) => {
-        time.split(",").forEach((t) => {
-          const [start, end] = t.split("-");
-
-          const row = document.createElement("div");
-          row.innerHTML = `
-            <label>Khung giờ ${index + 1}:</label>
-            <p>Từ <b>${start.trim()}</b> đến <b>${end.trim()}</b></p>
-          `;
-          scheduleContainer.appendChild(row);
+      document
+        .querySelector("#edit-schedule-btn")
+        .addEventListener("click", function () {
+          console.log(" this:", this.innerText);
+          if (this.innerText === "Edit") {
+            generateScheduleEdit(index, day);
+            this.innerText = "Add";
+          } else {
+            addSchedule();
+          }
         });
-      });
 
-      const selectHTML = document.getElementById("modal-trainer");
-      selectHTML.innerHTML =
-        ' <option value="" disabled selected>Chọn Trainer</option>';
-      trainerData.map((tr, index) => {
-        console.log(" trainerData.map ~ tr:", tr);
-        const option = document.createElement("option");
-        option.value = tr.name;
-        option.innerText = tr.name;
-        selectHTML.appendChild(option);
-      });
+      generateSchedule(index, day);
+
+      generateRegisterClass(1);
+
+      document
+        .querySelector("#session-quantity")
+        .addEventListener("change", function () {
+          generateRegisterClass(+this.value);
+        });
 
       M.Modal.getInstance(document.getElementById("studentModal")).open();
     });
@@ -231,37 +312,59 @@ function submitSchedule() {
   window.location.href = "confirm.html";
 }
 
-async function initTableData() {
-  // const scheduleData = JSON.parse(sessionStorage.getItem("scheduleData"));
-
-  try {
-    let response = await fetch(`${SCHEDULE_API_URL}?type=get_calendar`, {
-      redirect: "follow",
-      method: "GET",
-      headers: { "Content-Type": "text/plain;charset=utf-8" },
+const getStudentCalendar = async () => {
+  await fetch(`${SCHEDULE_API_URL}?type=get_calendar`, {
+    redirect: "follow",
+    method: "GET",
+    headers: { "Content-Type": "text/plain;charset=utf-8" },
+  })
+    .then(async (response) => {
+      const res = await response.json();
+      if (res.success) {
+        studentCalendar = parseSchedule(res.data);
+      }
+    })
+    .catch((error) => {
+      console.error("Lỗi khi lấy dữ liệu:", error);
     });
+};
 
-    const res = await response.json();
-    studentCalendar = parseSchedule(res.data);
-  } catch (error) {
-    console.error("Lỗi khi lấy dữ liệu:", error);
-  }
-}
-
-async function getAllUser() {
-  try {
-    const response = await fetch(`${ACCOUNT_API_URL}?type=get_all_user`, {
-      method: "GET",
-      redirect: "follow",
-      headers: { "Content-Type": "text/plain;charset=utf-8" },
+const getTrainerCalendar = async () => {
+  await fetch(`${SCHEDULE_API_URL}?type=get_trainer_calendar`, {
+    redirect: "follow",
+    method: "GET",
+    headers: { "Content-Type": "text/plain;charset=utf-8" },
+  })
+    .then(async (response) => {
+      const res = await response.json();
+      if (res.success) {
+        trainerCalendar = parseSchedule(res.data);
+        console.log(" .then ~ trainerCalendar:", trainerCalendar);
+      }
+    })
+    .catch((error) => {
+      console.error("Lỗi khi lấy dữ liệu:", error);
     });
-    const data = await response.json();
-    if (data.success) {
-      if (data.student) studentData = data.student;
-      if (data.trainer) trainerData = data.trainer;
-    }
-  } catch (error) {
-    console.error("Lỗi khi lấy dữ liệu:", error);
-  } finally {
-  }
-}
+};
+
+const getAllUser = async () => {
+  await fetch(`${ACCOUNT_API_URL}?type=get_all_user`, {
+    method: "GET",
+    redirect: "follow",
+    headers: { "Content-Type": "text/plain;charset=utf-8" },
+  })
+    .then(async (response) => {
+      const data = await response.json();
+      if (data.success) {
+        if (data.student) studentData = data.student;
+        if (data.trainer) trainerData = data.trainer;
+      }
+    })
+    .catch((error) => {
+      console.error("Lỗi khi lấy dữ liệu:", error);
+    });
+};
+
+const closeModal = () => {
+  M.Modal.getInstance(document.getElementById("studentModal")).close();
+};
